64066994b23092d9eb338162d044bceade7cacb3
duchoa2709 <21211tt4819@mail.tdc.edu.vn>
2023-10-25 15:23:41 +0700

    sua sidebar

--- wp-content/themes/twentytwentyone/template-parts/sidebar/sidebar-comment.php
+++ wp-content/themes/twentytwentyone/template-parts/sidebar/sidebar-comment.php
@@ -1,6 +1,6 @@
 <style>
 /* Sidebar styles */
-.sidebar {
+.sidebar-right {
     width: 344px;
     height: 100%;
     background-color: #fff;
@@ -8,11 +8,11 @@
     padding: 10px 15px;
 }
 
-.sidebar-comment {
+.sidebar-right .sidebar-comment {
     height: 100%;
 }
 
-.sidebar>h1 {
+.sidebar-right>h1 {
     position: relative;
     font-size: 20px;
     color: #444;
@@ -20,7 +20,7 @@
     text-shadow: none;
     display: inline-block;
 }
-.sidebar>h1:before{
+.sidebar-right>h1:before{
     position: absolute;
     content: "";
     width: 70%;
@@ -31,7 +31,7 @@
 }
 </style>
 
-<div class="sidebar w-1/4 rounded">
+<div class="sidebar-right w-1/4 rounded">
     <h1 class="font-bold mb-4">Comments</h1>
     <div class="sidebar-comment">
         <?php
@@ -39,7 +39,7 @@ $comments = get_comments(); // Lấy danh sách bình luận cho bài viết
 
 if ($comments) {
     foreach ($comments as $comment) {
-        echo '<div class="comment pl-1 py-1 border-b-2 text-[#488dc6] font-medium">';
+        echo '<div class="comment pl-3 py-1 border-b-2 text-[#488dc6] font-medium">';
         $comment_content = $comment->comment_content;
 
         // Giới hạn số kí tự hiển thị
---
+++ wp-content/themes/twentytwentyone/template-parts/sidebar/sidebar-none.php
@@ -0,0 +1,299 @@
+<style>
+.sidebar {
+    display: flex;
+    justify-content: space-between;
+}
+
+#sidebar-ben-phai .wp-block-latest-posts__post-title {
+    font-size: 10px;
+    /* Loại bỏ list-style và text-decoration */
+}
+
+.wp-block-latest-posts:not(.is-grid)>li {
+    margin-top: unset !important;
+    margin-bottom: unset !important;
+}
+
+#sidebar-ben-trai {
+    width: 100% !important;
+}
+
+#sidebar-ben-phai {
+    width: 30%;
+}
+
+#sidebar-ben-trai ul li {
+    position: relative;
+}
+
+#sidebar-ben-trai ul li a {
+    margin-left: 20px;
+}
+
+.box-category {
+    width: 100%;
+    float: left;
+    margin-bottom: 20px;
+}
+
+.box-xemnhieu .title-box-category {
+    border-bottom: 1px solid #e5e5e5;
+    padding-bottom: 0;
+    color: #757575;
+}
+
+.title-box-category {
+    display: block;
+    position: relative;
+    font-size: 18px;
+    line-height: 1.6;
+    margin-bottom: 12px;
+    font-family: "Merriweather", serif;
+    color: #222;
+    font-weight: bold;
+}
+
+.width_common {
+    width: 100%;
+    float: left;
+}
+
+.box-xemnhieu .list-top-view {
+    position: relative;
+    -ms-flex-wrap: wrap;
+    flex-wrap: wrap;
+}
+
+.box-xemnhieu .list-top-view:before {
+    content: "";
+    width: 1px;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 50%;
+    background: #e5e5e5;
+}
+
+.box-xemnhieu .list-top-view .item-news:nth-child(1) {
+    -webkit-box-ordinal-group: 2;
+    -ms-flex-order: 1;
+    order: 1;
+}
+
+.box-xemnhieu .list-top-view .item-news {
+    position: relative;
+    padding-left: 45px;
+    padding-right: 15px;
+    padding-bottom: 15px;
+    margin-bottom: 15px;
+    width: 50%;
+}
+
+.item-news {
+    width: 100%;
+    float: left;
+    padding-bottom: 15px;
+    margin-bottom: 15px;
+    border-bottom: 1px solid #e5e5e5;
+}
+
+.box-xemnhieu .list-top-view .number-top-view {
+    font-family: "Merriweather", serif;
+    font-size: 48px;
+    color: #222;
+    font-weight: bold;
+    position: absolute;
+    top: -14px;
+    left: 7px;
+    -webkit-font-feature-settings: "pnum"on, "lnum"on;
+    font-feature-settings: "pnum"on, "lnum"on;
+}
+
+.item-news .title-news {
+    font-family: "Merriweather", serif;
+    font-size: 14px;
+    line-height: 160%;
+    margin-bottom: 4px;
+}
+
+.box-xemnhieu .list-top-view .item-news:nth-child(1) {
+    -webkit-box-ordinal-group: 2;
+    -ms-flex-order: 1;
+    order: 1;
+}
+
+.box-xemnhieu .list-top-view .item-news {
+    position: relative;
+    padding-left: 45px;
+    padding-right: 15px;
+    padding-bottom: 15px;
+    margin-bottom: 15px;
+    width: 50%;
+}
+
+.item-news {
+    width: 100%;
+    float: left;
+    padding-bottom: 15px;
+    margin-bottom: 15px;
+    border-bottom: 1px solid #e5e5e5;
+}
+
+* {
+    margin: 0;
+    padding: 0;
+    -webkit-box-sizing: border-box;
+    box-sizing: border-box;
+}
+
+.item-news {
+    width: 100%;
+    height: 64px;
+    float: left;
+    padding-bottom: 15px;
+    margin-bottom: 15px;
+    border-bottom: 1px solid #e5e5e5;
+}
+
+.title-news {
+    height: 64px;
+}
+
+.headlines {
+    background: #56bdbf;
+    overflow: hidden;
+    padding: 20px 28px;
+    font-size: 15px;
+    font-weight: bold;
+}
+
+.headlines ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+}
+
+.headlines ul>li {
+    overflow: hidden;
+    display: flex;
+    align-items: center;
+    margin-bottom: 5px;
+    width: 100%;
+}
+
+.headlinesdate .headlinesdm,
+.news>.headlines .headlinesdate,
+.headlinesdate {
+    float: left;
+    font-family: 'Prata', serif;
+}
+
+.headlines .headlinesdate {
+    font-size: 0.8em;
+    min-width: 55px;
+    display: table-cell;
+    vertical-align: middle;
+}
+
+.headlines ul>li>.headlinestitle {
+    display: table-cell;
+    vertical-align: middle;
+    width: 85%;
+}
+
+.headlinesdate .headlinesdm,
+.news>.headlines .headlinesdate,
+.headlinesdate {
+    float: left;
+    font-family: 'Prata', serif;
+}
+
+.headlinesdate .headlinesyear {
+    line-height: 3.5em;
+    float: left;
+    margin-left: 3px;
+}
+
+.headlinesyear {
+    color: white;
+}
+
+.headlinesday {
+    border-bottom: 1px solid #fff;
+    color: white;
+}
+
+.headlinesmonth {
+    color: white;
+}
+
+.headlinesdate .headlinesday,
+.news>.headlines .headlinesmonth {
+    line-height: 1.7em;
+}
+
+.headlinesdate .headlinesday,
+.news>.headlines .headlinesmonth {
+    line-height: 1.7em;
+    color: white;
+}
+
+.headlines ul>li>.headlinestitle {
+    display: table-cell;
+    vertical-align: middle;
+    width: 85%;
+    color: white;
+}
+</style>
+<div class="sidebar w-1/4 rounded">
+    <div class="box-category box-xemnhieu ">
+        <hgroup class="width_common title-box-category">
+            <h2 class="parent-cate active"><a
+                    data-itm-source="#vn_source=Home&amp;vn_campaign=Box-XemNhieuNhat&amp;vn_medium=TitleBoxXemNhieuNhat&amp;vn_term=Desktop"
+                    title="Xem nhiều" href="/tin-xem-nhieu" class="inner-title" data-itm-added="1">Xem nhiều</a></h2>
+        </hgroup>
+        <div class="width_common list-top-view">
+            <?php 
+            $post_link = get_permalink(); 
+            $temp = 0;
+        ?>
+            <?php
+        $args = array(
+            'post_type' => 'post',
+            'posts_per_page' => -1,
+        );
+
+        $posts = get_posts($args);
+
+        if (!empty($posts)) {
+            foreach ($posts as $post) {
+                setup_postdata($post);
+                $temp = $temp + 1;
+                $post_link = get_permalink();
+        ?>
+
+            <article class="item-news">
+                <span class="number-top-view"><?php echo $temp ?></span>
+                <h3 class="title-news"><a href="<?php echo $post_link  ?>"
+                        data-itm-source="#vn_source=Home&amp;vn_campaign=Box-XemNhieuNhat&amp;vn_medium=Item-1&amp;vn_term=Desktop&amp;vn_thumb=0"
+                        title="Hiện trạng gần 150 căn nhà sai phép bị tháo dỡ ở TP HCM"
+                        data-itm-added="1"><?php the_title(); ?> </a>
+                    <span class="meta-news">
+                        <a class="count_cmt" href="<?php echo $post_link  ?>"
+                            style="white-space: nowrap; display: inline-block;">
+                            <svg class="ic ic-comment">
+                                <use xlink:href="#Comment-Reg"></use>
+                            </svg>
+                        </a>
+                    </span>
+                </h3>
+            </article>
+
+            <?php
+            }
+            wp_reset_postdata();
+        }
+        ?>
+        </div>
+    </div>
+</div>
